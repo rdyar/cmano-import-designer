6f77b56581cc0e876957158cf2a97b4396c0c035
Ron Dyar <ron@fromex.net>
2018-06-20 18:29:52 -0700

    Update impmaker.js

--- impmaker.js
+++ impmaker.js
@@ -8,12 +8,12 @@ function initialize() {
     zoom: 2,
     center: {lat: 0, lng: 0}
   });
-console.log("2");
+console.log("22");
   google.maps.event.addListener(map, 'click', function(event) {
       var marker = new google.maps.Marker({
             id: guid(),
             dbid: dbid.value,
-            position: event.latLng.lat(),
+            position: event.latLng,
             map: map,
             draggable: true,
             title: currtitle,
@@ -89,7 +89,7 @@ function genExport() {
             Member_GUID: markers[i].guid,
             Longitude: markers[i].position.F
         }
-console.log(markers[i].position.F);
+console.log(markers[i].position.lat());
         inst.MemberRecords.push(member);
     }
 
